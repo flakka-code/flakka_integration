3820f41010e4c5c3d81370fb50701efa456308fb
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-27 04:23:27 -0500

    Initial commit

---
+++ justfile
@@ -0,0 +1,5 @@
+scaffold-repo:
+  gh repo create flakka-code/flakka_integration --clone --public
+
+scaffold-this:
+  very_good create dart_cli flakka_integration -o ../
---
+++ lib/flakka_integration.dart
@@ -0,0 +1,10 @@
+/// flakka_integration, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate flakka_integration
+/// dart pub global activate flakka_integration
+///
+/// # see usage
+/// flakka_integration --help
+/// ```
+library;
